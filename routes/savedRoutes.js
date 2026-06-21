63b887ee0d1d86af7a1a5ee30bc3df6b216f7e76
Sudhanshu7808 <sudhanshukumar780820@gmail.com>
2026-06-21 19:30:05 +0530

    Saving properties

--- routes/savedRoutes.js
+++ routes/savedRoutes.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 
-const User = require("../models/User");
+const User = require("../models/user");
 
 router.post("/:plotId", authMiddleware, async (req, res) => {
   try {
